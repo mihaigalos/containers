585af78d3273a0c68ddf636e373b37b4d058bc2a
Mihai Galos <mihaigalos@gmail.com>
2020-05-07 22:21:33 +0200

    Multiple add and read random2 working

--- test/unit/test_basic.cpp
+++ test/unit/test_basic.cpp
@@ -73,3 +73,24 @@ TEST_F(Fixture, AddMultiple_WhenRandom)
     ASSERT_EQ(sut_[2].i, 5);
     ASSERT_EQ(sut_[2].j, 6);
 }
+
+TEST_F(Fixture, AddMultiple_WhenRandom2)
+{
+
+    sut_[0] = DemoStructure{1, 2};
+    sut_[2] = DemoStructure{5, 6};
+    sut_[4] = DemoStructure{9, 10};
+    sut_[1] = DemoStructure{3, 4};
+    sut_[3] = DemoStructure{7, 8};
+
+    ASSERT_EQ(sut_[0].i, 1);
+    ASSERT_EQ(sut_[0].j, 2);
+    ASSERT_EQ(sut_[1].i, 3);
+    ASSERT_EQ(sut_[1].j, 4);
+    ASSERT_EQ(sut_[2].i, 5);
+    ASSERT_EQ(sut_[2].j, 6);
+    ASSERT_EQ(sut_[3].i, 7);
+    ASSERT_EQ(sut_[3].j, 8);
+    ASSERT_EQ(sut_[4].i, 9);
+    ASSERT_EQ(sut_[4].j, 10);
+}
