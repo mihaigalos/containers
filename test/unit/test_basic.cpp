d35ee81c1bbe8a47b51a811403e1ab40072f1e70
Mihai Galos <mihaigalos@gmail.com>
2020-05-07 21:13:55 +0200

    Improve cohesion

--- test/unit/test_basic.cpp
+++ test/unit/test_basic.cpp
@@ -14,21 +14,19 @@ class Fixture : public ::testing::Test
 public:
 protected:
     virtual void SetUp() override {}
+
+    uint16_t expected_size{10};
+    containers::map<DemoStructure, decltype(expected_size)> sut_{expected_size};
 };
 
 TEST_F(Fixture, ConstructWorks_WhenTypical)
 {
-    auto expected_size{10};
-    containers::map<DemoStructure, decltype(expected_size)> sut_{expected_size};
 
     ASSERT_EQ(sut_.size(), expected_size);
 }
 
 TEST_F(Fixture, ClearWorks_WhenTypical)
 {
-    auto initial_size{10};
-    containers::map<DemoStructure, decltype(initial_size)> sut_{initial_size};
-
     sut_.clear();
 
     ASSERT_EQ(sut_.size(), 0);
@@ -36,9 +34,6 @@ TEST_F(Fixture, ClearWorks_WhenTypical)
 
 TEST_F(Fixture, AddWorks_WhenTypical)
 {
-    auto initial_size{10};
-    containers::map<DemoStructure, decltype(initial_size)> sut_{initial_size};
-
     sut_[0] = DemoStructure{1, 2};
     auto actual = sut_[0];
 
