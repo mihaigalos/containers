1765ca7e341ea86310adbf15b903b1d68e9f5951
Mihai Galos <mihaigalos@gmail.com>
2020-05-07 22:47:39 +0200

    Add demo for avr

---
+++ src/avr_main.cpp
@@ -0,0 +1,34 @@
+#include "include/containers/map.h"
+
+#include <avr/io.h>
+
+void operator delete(void *ptr, unsigned int foo)
+{
+    delete ptr;
+}
+
+void operator delete(void *ptr)
+{
+    delete ptr;
+}
+
+struct DemoStructure
+{
+    uint8_t i;
+    uint8_t j;
+};
+
+uint16_t expected_size{10};
+containers::map<DemoStructure, decltype(expected_size)> sut_{expected_size};
+
+int main()
+{
+
+    sut_[0] = DemoStructure{1, 2};
+    auto actual = sut_[0];
+
+    if (actual.i & 0x55)
+    {
+        PORTB |= (1 << 5);
+    }
+}
