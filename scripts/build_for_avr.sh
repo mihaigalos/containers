f9de9926a9a7ada9c5a4836b3e603a5f1ccd9af3
Mihai Galos <mihaigalos@gmail.com>
2020-05-10 21:09:04 +0200

    Build with avr-gcc -std=c++14 since c++17 not supported

--- scripts/build_for_avr.sh
+++ scripts/build_for_avr.sh
@@ -4,7 +4,7 @@ pushd ../demo
 for file in *.cpp; do
 
 file_without_extension="${file%.*}"
-avr-gcc -std=c++17  -Os -mmcu=atmega328p "${file_without_extension}".cpp -o "${file_without_extension}".elf
+avr-gcc -std=c++14  -Os -mmcu=atmega328p "${file_without_extension}".cpp -o "${file_without_extension}".elf
 
 avr-size "${file_without_extension}".elf
 avr-nm --size-sort -C -r --radix=d "${file_without_extension}".elf
