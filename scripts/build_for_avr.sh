6fd76f22d93fe521e01f9cf8038db64be2643255
Mihai Galos <mihaigalos@gmail.com>
2020-05-10 21:30:30 +0200

    Reenable bazel, add fails on avr-size and avr-nm, make script verbose

--- scripts/build_for_avr.sh
+++ scripts/build_for_avr.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 function err() {
     echo -e "\e[1;31m${@}\e[0m" >&2
     exit 1
 }
 
 pushd ../demo
+echo ""
+
 for file in *.cpp; do
 
 file_without_extension="${file%.*}"
 avr-gcc -std=c++14  -Os -mmcu=atmega328p "${file_without_extension}".cpp -o "${file_without_extension}".elf  || err "Compiling ${file_without_extension} failed."
 
-avr-size "${file_without_extension}".elf
-avr-nm --size-sort -C -r --radix=d "${file_without_extension}".elf
+avr-size "${file_without_extension}".elf || err "Computing size of ${file_without_extension} failed."
+avr-nm --size-sort -C -r --radix=d "${file_without_extension}".elf || err "Listing functional decomposition of ${file_without_extension} failed."
 
 echo ""
 
