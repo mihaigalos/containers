89394d809573ef7f5c4ea0785396015d7bd941ab
Mihai Galos <mihaigalos@gmail.com>
2020-05-16 21:34:51 +0200

    Remove superfluous

--- include/containers/size_wrapper.h
+++ include/containers/size_wrapper.h
@@ -14,9 +14,5 @@ struct conditional<false, T, F>
     using type = F;
 };
 
-template <unsigned n>
-using FittingUIntT = conditional<
-    n <= UINT8_MAX, uint8_t, conditional<n <= UINT16_MAX, uint16_t, conditional<n <= UINT32_MAX, uint32_t, uint64_t>>>;
-
 template <unsigned value>
 using Size_t_impl = typename conditional<(value > 255), uint16_t, uint8_t>::type;
