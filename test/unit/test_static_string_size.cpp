6128ed60e2e108bd9bad038a88d1affee19a9eb0
Mihai Galos <mihaigalos@gmail.com>
2020-05-16 21:41:58 +0200

    Remove superfluous, test at edge

--- test/unit/test_static_string_size.cpp
+++ test/unit/test_static_string_size.cpp
@@ -4,19 +4,13 @@
 
 #include "containers/static_string.h"
 
-#include "test_common.h"
-
-class Fixture : public ::testing::Test
+TEST(Fixture, ValueTypeOfSizeUINT16Works_WhenTypical)
 {
-};
-
-TEST_F(Fixture, ValueTypeOfSizeUINT16Works_WhenTypical)
-{
-    constexpr uint16_t uint16_value{300};
+    constexpr uint16_t uint16_value{65535};
     static_assert(std::is_same<containers::static_string<uint16_value>, containers::__static_string<uint16_t, uint16_value>>::value, "");
 }
 
-TEST_F(Fixture, ValueTypeOfSizeUINT8Works_WhenTypical)
+TEST(Fixture, ValueTypeOfSizeUINT8Works_WhenTypical)
 {
     constexpr uint8_t uint8_value{255};
     static_assert(std::is_same<containers::static_string<uint8_value>, containers::__static_string<uint8_t, uint8_value>>::value, "");
