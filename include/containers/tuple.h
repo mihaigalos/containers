b2d636f237468133e0be62573c2d3a6c8e0faf60
Mihai Galos <mihaigalos@gmail.com>
2020-06-01 22:45:06 +0200

    Implement containers::tuple

---
+++ include/containers/tuple.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "utilities.h"
+
+namespace containers
+{
+
+template <typename T1, typename T2>
+class tuple
+{
+public:
+    T1 t1;
+    T2 t2;
+
+    template <typename Tr1, typename Tr2>
+    tuple(Tr1 &&first, Tr2 &&second) : t1(forward<Tr1>(first)), t2(forward<Tr2>(second)) {}
+
+    template <typename Tr1, typename Tr2>
+    tuple<T1, T2> &operator=(const tuple<Tr1, Tr2> &other)
+    {
+        t1 = other.t1;
+        t2 = other.t2;
+        return *this;
+    }
+};
+
+template <typename T1, typename T2>
+tuple<T1 &, T2 &> tie(T1 &first, T2 &second)
+{
+    return tuple<T1 &, T2 &>(first, second);
+}
+
+} // namespace containers
---
+++ include/containers/utilities.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace containers
+{
+
+template <typename T, typename U>
+constexpr decltype(auto) forward(U &&u) noexcept
+{
+    return static_cast<T &&>(u);
+}
+
+} // namespace containers
